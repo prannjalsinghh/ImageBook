ff34773c9be5783d6b2a9ffc556ca8542bb17c37
Pranjal <pranjalsingh531@gmail.com>
2023-07-14 17:12:21 +0530

    updated links

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f1ag36g.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2awtpi7.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(uri, () => console.log('connected'));
 
 app.use('/', require('./Routes/appRouter'))
